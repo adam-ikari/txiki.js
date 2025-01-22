9b0ed430849dceac540a84618af56416212b9756
adam <chenzd123456@gmail.com>
2025-01-22 13:57:52 +0800

    feat: update SDL initialization to accept flags and improve window creation

--- examples/sdl_test.js
+++ examples/sdl_test.js
@@ -1,7 +1,7 @@
 import { init, createWindow, quit, pollEvents } from 'tjs:sdl';
 
 try {
-    init();
+    init(0x00004020); // SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS
     const win = createWindow("SDL Test - Click X to Close", 100, 100, 640, 480, 0);
     console.log("Window created successfully!");
     
